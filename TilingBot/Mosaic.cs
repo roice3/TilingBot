4409170f3c85d276bd518cdfdc1415c0391afb1e
Roice Nelson <roice3@gmail.com>
2019-07-30 23:20:57 -0500

    Mosaic code.

--- TilingBot/Mosaic.cs
+++ TilingBot/Mosaic.cs
@@ -2,7 +2,10 @@
 {
 	using System.Collections.Generic;
 	using System.Drawing;
+	using System.Drawing.Imaging;
 	using System.IO;
+	using R3.Core;
+	using R3.Drawing;
 	using R3.Geometry;
 
 	internal class Mosaic
@@ -10,8 +13,84 @@
 		public void Generate()
 		{
 			string mosaicDir = @"D:\GitHub\TilingBot\TilingBot\working\mosaic";
-			string[] files = EnumFiles( new string[] { @"D:\Dropbox\TilingBot\tweeted", @"D:\TilingBot\working\queue" } );
-			GenImages( mosaicDir, files );
+			string[] files = EnumFiles( new string[] { @"D:\GitHub\TilingBot\TilingBot\working\experiment graveyard\mosaic" } );
+			//GenImages( mosaicDir, files );
+			//return;
+
+			// Load image info.
+			mosaicDir = @"D:\GitHub\TilingBot\TilingBot\working\mosaic";
+			var images = LoadImages( mosaicDir );
+			//return;
+
+			string sourceImage = @"D:\GitHub\TilingBot\TilingBot\working\tet_50.png";
+			Bitmap source = new Bitmap( sourceImage );
+
+			List<string> inputImages = new List<string>();
+			for( int x = 0; x < source.Width; x++ )
+			for( int y = 0; y < source.Height; y++ )
+			{
+				Color c = source.GetPixel( x, y );
+				ImageInfo closest = FindClosest( c, images.Values );
+				closest.Used = true;
+				string fn = Path.GetFileName( closest.Path );
+				inputImages.Add( fn );
+				System.Diagnostics.Trace.WriteLine( fn + "\t" + c.GetBrightness() + "\t" + closest.AvgBrightness + "\t" + c.GetSaturation() + "\t" + closest.AvgSat );
+			}
+
+			// Create output image.
+			int numTiles = source.Width;
+			//numTiles = 10;
+			int gridSpace = 30;
+			int tileSize = 300;
+			int size = numTiles * tileSize + ( numTiles + 1 ) * gridSpace;
+
+			ImageGrid.Settings settings = new ImageGrid.Settings()
+			{
+				Directory = mosaicDir,
+				Rows = numTiles,
+				Columns = numTiles,
+				Width = size,
+				Height = size,
+				hGap = gridSpace,
+				vGap = gridSpace,
+				InputImages = inputImages.ToArray(),
+				FileName = "mosaic.png"
+			};
+
+			ImageGrid grid = new ImageGrid();
+			grid.Generate( settings );
+		}
+
+		private ImageInfo FindClosest( Color c, IEnumerable<ImageInfo> images )
+		{
+			// Use brightness and saturation as a measure of closeness.
+			double satScale = 0.4;
+			double cBrightness = c.GetBrightness();
+			double cSat = c.GetSaturation() * satScale;
+			System.Func<double, double, double> distFn = ( brightness, sat ) =>
+			  {
+				  sat *= satScale;
+				  return new Vector3D( cBrightness, cSat ).Dist( new Vector3D( brightness, sat ) );
+			  };
+
+			double dist = double.MaxValue;
+
+			ImageInfo closest = null;
+			foreach( ImageInfo image in images )
+			{
+				if( image.Used )
+					continue;
+				if( !File.Exists( image.Path ) )
+					continue;
+
+				double d = distFn( image.AvgBrightness, image.AvgSat );
+				if( d < dist )
+				{
+					dist = d;
+					closest = image;
+				}
+			}
+			return closest;
 		}
 
 		private string[] EnumFiles( string[] directories )
@@ -24,54 +103,31 @@
 
 		private void GenImages( string mosaicDir, string[] files )
 		{
-			int count = 0;
+			List<int[]> cData = new List<int[]>();
+			//cData.Add( new int[] { 40, 4 } );
+			cData.Add( new int[] { 30, 18 } );
+			cData.Add( new int[] { 40, 36 } );
+			cData.Add( new int[] { 20, 5 } );
+
+			int count = 4000;//0;
 			foreach( string file in files )
 			{
 				Tiler.Settings settings = Persistence.LoadSettings( file );
 				if( settings.Geometry != Geometry.Hyperbolic )
 					continue;
 
-				foreach( HyperbolicModel hModel in this.hModels )
+				//if( settings.ColoringOption != 3 )
+				//	continue;
+				//foreach( int[] data in cData )
 				{
-					settings.HyperbolicModel = hModel;
-					//Program.StandardInputs( settings );
-					//GenImage( mosaicDir, settings, count++ );
-					count++;
+					//settings.ColoringData = new int[] { settings.ColoringData == null ? 0 : settings.ColoringData[0], data[0], data[1] };
+					foreach( HyperbolicModel hModel in this.hModels )
+					{
+						settings.HyperbolicModel = hModel;
+						Program.StandardInputs( settings );
+						GenImage( mosaicDir, settings, count++ );
+					}
 				}
-			}
-		}
-
-		private SphericalModel[] sModels
-		{
-			get
-			{
-				return new SphericalModel[]
-				{
-					SphericalModel.Sterographic,
-					SphericalModel.Gnomonic,
-					SphericalModel.Azimuthal_Equidistant,
-					SphericalModel.Equirectangular,
-					SphericalModel.Mercator,
-					SphericalModel.Orthographic,
-					SphericalModel.Sinusoidal,
-					SphericalModel.PeirceQuincuncial,
-				};
-			}
-		}
-
-		private EuclideanModel[] eModels
-		{
-			get
-			{
-				return new EuclideanModel[]
-				{
-					EuclideanModel.Isometric,
-					EuclideanModel.Conformal,
-					EuclideanModel.Disk,
-					EuclideanModel.UpperHalfPlane,
-					EuclideanModel.Spiral,
-					EuclideanModel.Loxodromic,
-				};
 			}
 		}
 
@@ -106,18 +162,74 @@
 			Program.MakeTiling( settings );
 		}
 
-		public class BitmapInfo
+		private Dictionary<string, ImageInfo> LoadImages( string dir )
 		{
-			Bitmap m_bitmap;
-			int m_intensity;
+			HashSet<double> dupChecker = new HashSet<double>( new DoubleEqualityComparer( 0.0001 ) );
+			Dictionary<string, ImageInfo> images = new Dictionary<string, ImageInfo>();
+			string cached = Path.Combine( dir, "list.txt" );
+			if( File.Exists( cached ) )
+			{
+				string[] lines = File.ReadAllLines( cached );
+				foreach( string line in lines )
+				{
+					string[] split = line.Split( '\t' );
+					ImageInfo ii = new ImageInfo()
+					{
+						Path = split[0],
+						AvgBrightness = double.Parse( split[1] ),
+						AvgSat = double.Parse( split[2] )
+					};
+					if( dupChecker.Add( ii.AvgBrightness ) )
+						images[ii.Path] = ii;
+				}
+			}
+			else
+			{ 		
+				foreach( string file in Directory.EnumerateFiles( dir, "*.png", SearchOption.TopDirectoryOnly ) )
+				{
+					ImageInfo ii = LoadBitmap( file );
+					images[ii.Path] = ii;
+				}
+			}
 
+			return images;
 		}
 
-		public static BitmapInfo LoadBitmap( string path )
+		public class ImageInfo
 		{
+			public Bitmap Bitmap { get; set; }
+			public string Path { get; set; }
+			public double AvgBrightness { get; set; }
+			public double AvgSat { get; set; }
+
+			public bool Used = false;
+		}
+
+		public static ImageInfo LoadBitmap( string path )
+		{
+			double avgBrightness = 0, avgSat = 0;
 			Bitmap bitmap = new Bitmap( path );
-			//bitmap.
-			throw new System.NotImplementedException();
+			for( int x = 0; x < bitmap.Width; x++ )
+			for( int y = 0; y < bitmap.Height; y++ )
+			{
+				Color c = bitmap.GetPixel( x, y );
+				avgBrightness += c.GetBrightness();
+				avgSat += c.GetSaturation();
+			}
+
+			int numPixels = bitmap.Width * bitmap.Height;
+			avgBrightness /= numPixels;
+			avgSat /= numPixels;
+			bitmap.Dispose();
+
+			System.Diagnostics.Trace.WriteLine( path + "\t" + avgBrightness + "\t" + avgSat );
+			return new ImageInfo()
+			{
+				//Bitmap = bitmap,
+				Path = path,
+				AvgBrightness = avgBrightness,
+				AvgSat = avgSat
+			};
 		}
 	}
 }
